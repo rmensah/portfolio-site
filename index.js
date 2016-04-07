3a87773be1cce0059363a964ba737bbb681dd6a5
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-04-07 14:42:24 -0400

    initial commit of index.js package.json and public dir

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+
+app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", function(req,res){
+	res.sendFile(process.cwd() + "/public/index.html")
+})
+
+var port = 3000;
+app.listen(port, function() {
+  console.log("listening on port:" + port);
+});
