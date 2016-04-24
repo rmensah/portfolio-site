4ffbee4864c121d143a579c6bd89625b9ec7affe
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-04-23 21:59:33 -0400

    debugged index.js to fix heroku deploy error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("*", function(req,res){
 	res.sendFile(process.cwd() + "/public/index.html")
-})
+});
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
